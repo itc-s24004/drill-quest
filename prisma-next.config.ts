c98e18644497e22565d52aa64571b356fbca0f53
s24004 <s24004@std.it-college.ac.jp>
2026-07-28 09:26:21 +0900

    prisma-nextをprismaに変更

--- prisma-next.config.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { defineConfig } from '@prisma-next/postgres/config';
-
-export default defineConfig({
-  contract: "./prisma/contract.prisma",
-  db: {
-    connection: process.env['DATABASE_URL']!,
-  },
-});
