244147cb6c422f67bef0b07d27dce9aac4503d5b
s24004 <s24004@std.it-college.ac.jp>
2026-08-08 09:17:37 +0900

    トップページをブックマーク機能に対応

--- app/page.client.tsx
+++ app/page.client.tsx
@@ -1,29 +1,36 @@
 "use client"
 
 
+import { useState } from "react"
 import { ClientSide } from "./_components/app/client/component"
 import QuizCard from "./_components/QuizCard"
 import SearchBar from "./_components/SearchBar"
-import { App_DB_Drill } from "./app.type"
+import { App_DB_Drill, App_DB_Drill_ } from "./app.type"
 
 type ClientScreenProps = {
-    data: App_DB_Drill[]
+    data: App_DB_Drill_[]
 }
 
 export function ClientScreen({data}: ClientScreenProps) {
+    const [Drills, setDrills] = useState(data)
+    
     return (
         <ClientSide>
             <div className="flex flex-col gap-4 px-4 py-4">
                 <SearchBar onSearch={() => {}}/>
 
                 <div className="flex flex-col gap-3">
-                {data.map((quiz) => (
+                {Drills.map((quiz) => (
                     <QuizCard
+                        data={quiz}
                         key={quiz.id}
-                        title={quiz.title}
-                        description={quiz.description}
-                        likeCount={0}
-                        starCount={0}
+                        onUpdate={(newData) => {
+                            setDrills((currentDrills) => {
+                                const newDrills = [...currentDrills];
+                                newDrills.splice(newDrills.indexOf(quiz), 1, newData);
+                                return newDrills;
+                            })
+                        }}
                     />
                 ))}
                 </div>
