0ad4560d561854d10a5808d3e0acaff2079951bc
s24004 <s24004@std.it-college.ac.jp>
2026-08-08 09:13:07 +0900

    アプリで使用する型を追加

--- app/app.type.d.ts
+++ app/app.type.d.ts
@@ -92,8 +92,146 @@ export type App_DB_Drill = Prisma.DrillGetPayload<{
                 }
             }
         };
+        _count: {
+            select: {
+                bookmark;
+            }
+        }
     }
 }>
+
+
+type a = Prisma.DrillGetPayload<{
+    select: {
+        id;
+        title;
+        description;
+        drillTag: {
+            select: {
+                id;
+                tag: {
+                    select: {
+                        id;
+                        name;
+                    }
+                }
+            }
+        };
+        _count: {
+            select: {
+                bookmark: true;
+            }
+        };
+        bookmark: {
+            where: {
+                userId;
+            };
+        }
+    };
+}>
+
+
+
+
+
+
+type App_DB_Category = Prisma.CategoryGetPayload<{
+    select: {
+        id: true;
+        name: true;
+    }
+}>
+
+
+
+
+
+
+type App_DB_Drill_ = Prisma.DrillGetPayload<{
+    select: {
+        id: true;
+        title: true;
+        description: true;
+        drillTag: {
+            select: {
+                id: true;
+                tag: {
+                    select: {
+                        id: true;
+                        name: true;
+                    }
+                }
+            }
+        };
+        _count: {
+            select: {
+                bookmark: true;
+            }
+        };
+        bookmark: {
+            where: {
+                user: {
+                    id: true;
+                }
+            };
+            select: {
+                id: true;
+            }
+        }
+    }
+}>
+
+
+type App_DB_Drill_Detail = Prisma.DrillGetPayload<{
+    select: {
+        id: true;
+        title: true;
+        description: true;
+        drillTag: {
+            select: {
+                id: true;
+                tag: {
+                    select: {
+                        id: true;
+                        name: true;
+                    }
+                }
+            }
+        };
+        questions: {
+            select: {
+                id: true;
+                body: true;
+                sortIndex: true;
+                choices: {
+                    select: {
+                        id: true;
+                        body: true;
+                        isCorrect: true;
+                    }
+                }
+            }
+        };
+        bookmark: {
+            where: {
+                user: {
+                    id: true;
+                }
+            };
+            select: {
+                id: true;
+            }
+        }
+    }
+}>;
+
+
+type App_DB_Bookmark = Prisma.BookmarkGetPayload<{
+    select: {
+        id: true;
+    }
+}>
+
 
 // type a = Prisma.GroupModel[""]
 
