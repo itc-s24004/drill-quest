d4c13ace6f23757bf10cfab30dc20ba95c0c95b2
s24004 <s24004@std.it-college.ac.jp>
2026-08-12 11:43:34 +0900

    ヘッダー内の処理を呼び出し元で処理するように変更

--- app/_components/app/header/component.tsx
+++ app/_components/app/header/component.tsx
@@ -6,16 +6,29 @@ import { app_api_request_get } from "@/app/api/app/app.api.type";
 import { app_api_drill } from "@/app/api/app/drill/route";
 import { User } from "lucide-react";
 import Link from "next/link";
+import { StateUpdateListener } from "@/app/_lib/client/update/state";
 
 
 
 type HeaderProps = {
     categories?: App_DB_Category[] | undefined;
-    query?: app_api_request_get<app_api_drill, "searchDrills"> | undefined;
+    query: app_api_request_get<app_api_drill, "searchDrills">;
+    onUpdateQuery?(query: app_api_request_get<app_api_drill, "searchDrills">): void;
 }
 
 
-export function Header({categories, query}: HeaderProps) {
+export function Header({categories, query, onUpdateQuery}: HeaderProps) {
+    const [currentQuery, setCurrentQuery] = useState(query);
+
+
+    StateUpdateListener({value: query, onUpdate() {
+        setCurrentQuery(query);
+    }});
+
+
+
+
+
     const [keyword, setKeyword] = useState("");
     const [category, setCategory] = useState<number>();
 
@@ -28,7 +41,13 @@ export function Header({categories, query}: HeaderProps) {
                     <User size={18} color="#6b7280" strokeWidth={1.8} />
                 </Link>
                 <div className="flex-1">
-                    <SearchBar onSearch={(q) => setKeyword(q)} />
+                    <SearchBar keywork={currentQuery.title ?? ""}
+                        onChangeKeywork={(keyword) => {
+                            setCurrentQuery({...currentQuery, title: keyword})
+                        }}
+                        onSubmit={() => {
+                            onUpdateQuery?.(currentQuery);
+                        }}/>
                 </div>
             </div>
             {
