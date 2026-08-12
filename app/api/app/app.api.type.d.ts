6ff9becbc455e5fdac509f3d479803ff0263abd7
s24004 <s24004@std.it-college.ac.jp>
2026-08-12 09:46:14 +0900

    apiの型定義とクライアントを追加

--- app/api/app/app.api.type.d.ts
+++ app/api/app/app.api.type.d.ts
@@ -22,6 +22,9 @@ type app_api_doc_get = {
     res: app_api_response
 }
 
+
+
+
 export type app_api_map<map extends Record<string, app_api_doc_get> = Record<string, app_api_doc_get>> = map;
 
 
@@ -29,3 +32,39 @@ export type app_api_map<map extends Record<string, app_api_doc_get> = Record<str
 export type app_api_response_get<api_map extends app_api_map, type extends keyof api_map> = api_map[type]["res"]
 
 export type app_api_request_get<api_map extends app_api_map, type extends keyof api_map> = api_map[type]["req"]
+
+
+
+
+
+type app_api_req<body> = {
+    params: Record<string,
+        string | string[] |
+        number | number[] |
+        boolean | boolean[] |
+        Date | Date[] |
+        undefined | undefined[]
+    > | undefined;
+    body: body | undefined;
+}
+
+
+type app_api_doc<body> = {
+    req: {
+        params: Record<string,
+            string | string[] |
+            number | number[] |
+            boolean | boolean[] |
+            Date | Date[] |
+            undefined | undefined[]
+        > | undefined;
+        body: body | undefined
+    };
+    res: app_api_response;
+}
+export type app_api_map2<map extends Record<string, app_api_doc> = Record<string, app_api_doc>> = map;
+
+
+export type app_api_response_get2<api_map extends app_api_map2, type extends keyof api_map> = api_map[type]["res"]
+
+export type app_api_request_get2<api_map extends app_api_map2, type extends keyof api_map> = api_map[type]["req"]
--- app/api/app/client.ts
+++ app/api/app/client.ts
@@ -1,10 +1,11 @@
 "use client"
 
 
-import { app_api_map, app_api_response } from "./app.api.type";
+import { app_api_map, app_api_map2, app_api_response } from "./app.api.type";
 import { App_API_Drill } from "./drill/client";
 import { HTTP_METHOD } from "next/dist/server/web/http";
 import { App_API_Bookmark } from "./bookmark/client";
+import { App_API_Answer } from "./answer/client";
 
 
 export async function api_get<api_map extends app_api_map, type extends keyof api_map, raw_response extends api_map[type]["res"] = api_map[type]["res"], response = app_api_response<raw_response> | undefined >(path: string, searchparams: api_map[type]["req"]): Promise<response> {
@@ -96,6 +97,29 @@ export async function api_client_req<api_map extends app_api_map, type extends k
 
         }
     }
+}
+
+
+
+
+
+export async function api_client_req2<api_map extends app_api_map2, type extends keyof api_map, raw_response extends api_map[type]["res"] = api_map[type]["res"] >(path: string, method: HTTP_METHOD, params: api_map[type]["req"]["params"], body: api_map[type]["req"]["body"]): Promise<raw_response | undefined> {
+    const url = new URL(path, window.location.origin);
+    const p = url.searchParams;
+
+    if (params) Object.entries(params).forEach(([key, value]) => {
+        p.set(key, String(value))
+    });
+
+    return await fetch(url, { method, body: JSON.stringify(body) }).then(async res => {
+        try {
+            return await res.json();
+            
+        } catch {
+            return;
+
+        }
+    }).catch(() => undefined);
 }
 
 
