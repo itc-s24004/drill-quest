a58e4a11f8d8a493185a3711b76164f548ecac78
s24004 <s24004@std.it-college.ac.jp>
2026-08-12 12:08:01 +0900

    ブックマークの状態取得にセッションが反映されていない問題を修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
+import { getServerSession } from "next-auth";
 import { DrillView } from "./_components/app/drillView/component";
 import { DB_Util } from "./_lib/server/db/util";
 import { App_DB_Drill_ } from "./app.type";
-import { getSession } from "next-auth/react";
 
 
 export default async function HomePage() {
-    const session = await getSession();
+    const session = await getServerSession();
     
     
     const drills: App_DB_Drill_[] = await DB_Util.Drill.getDrills_({
         requestUserEmail: session?.user?.email ?? undefined,
         max: 30
-    }) ?? []
+    }) ?? [];
     
     return (
         <DrillView data={drills} />
